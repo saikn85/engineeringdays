9d5bc425f73fa07c53f6e30d6a98eb352b691b66
saikn85 <85madmaxfury@gmail.com>
2023-01-11 23:22:22 +0530

    day 3

---
+++ CSharp/Chapter3/Problem36.cs
@@ -0,0 +1,12 @@
+﻿namespace EngineeringDays.Chapter3;
+
+internal static class Problem36
+{
+    public static void ConvertInchToInchMetrics(double measure = 1)
+    {
+        Console.WriteLine($"inch: {measure:0.00000} = foot: {measure / 12:0.00000}");
+        Console.WriteLine($"inch: {measure:0.00000} = yard: {measure / 36:0.00000}");
+        Console.WriteLine($"inch: {measure:0.00000} = centimeter: {measure * 2.54:0.00000}");
+        Console.WriteLine($"inch: {measure:0.00000} = meter: {measure / 39.37:0.00000}");
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,3 +4,4 @@ Problem28.GetRightMostDigit();
 Problem29.GetSecondRightMostDigit();
 Problem30.CalculateAreaPerimeterOfRectangle(5, 2);
 Problem31.RadiansToDegrees(1);
+Problem36.ConvertInchToInchMetrics(39.37);
