024ba47041889fb5752c5c041a652e44df68819c
saikn85 <85madmaxfury@gmail.com>
2023-01-23 11:12:25 +0530

    day 8

--- CSharp/Chapter4/Problem44.cs
+++ CSharp/Chapter4/Problem44.cs
@@ -9,24 +9,6 @@ internal static class Problem44
     {
         ReadValues(out ushort length, out ushort width, out decimal discount, out decimal costPerSqFt);
         GenerateBill(length, width, costPerSqFt, discount);
-        
-
-
-        StringBuilder sb = new();
-        //sb.AppendLine("Description\tQuantity\tUnit Price\tTotal Price");
-        //sb.AppendLine("-------------\t--------\t----------\t-----------");
-        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "TV", tvUnitsSold, tvPrice, tvTotal));
-        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "VCR", vcrUnitsSold, vcrPrice, vcrTotal));
-        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "RC", rcUnitsSold, rcPrice, rcTotal));
-        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "CD", cdUnitsSold, cdPrice, cdTotal));
-        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "Tape Recorder", tpUnitsSold, tpPrice, tpTotal));
-        //sb.AppendLine("-------------\t--------\t----------\t-----------");
-        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, -10:N2}\t{3, 11:N2}", "", "", "Sub Total", subTotal));
-        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, -10:N2}\t{3, 11:N2}", "", "", "Tax", tax));
-        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, -10:N2}\t{3, 11:N2}", "", "", "Total", total));
-
-        WriteLine("The Report");
-        WriteLine(sb.ToString());
     }
 
     private static void ReadValues(out ushort length, out ushort width, out decimal discount, out decimal costPerSqFt)
@@ -68,5 +50,27 @@ internal static class Problem44
         decimal taxed = subtotal * (tax / 100);
         decimal total = subtotal + taxed;
 
+        PrintReport(costPerSqFt, laborCost, carpet, totalLaborCost, installedPrice, discountedCost, subtotal, taxed, total);
+    }
+
+    private static void PrintReport(decimal costPerSqFt, decimal laborCost, decimal carpet, 
+        decimal totalLaborCost, decimal installedPrice, decimal discountedCost, 
+        decimal subtotal, decimal taxed, decimal total)
+    {
+        StringBuilder sb = new();
+        //sb.AppendLine("Description\tQuantity\tUnit Price\tTotal Price");
+        //sb.AppendLine("-------------\t--------\t----------\t-----------");
+        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "TV", tvUnitsSold, tvPrice, tvTotal));
+        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "VCR", vcrUnitsSold, vcrPrice, vcrTotal));
+        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "RC", rcUnitsSold, rcPrice, rcTotal));
+        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "CD", cdUnitsSold, cdPrice, cdTotal));
+        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, 10:N2}\t{3, 11:N2}", "Tape Recorder", tpUnitsSold, tpPrice, tpTotal));
+        //sb.AppendLine("-------------\t--------\t----------\t-----------");
+        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, -10:N2}\t{3, 11:N2}", "", "", "Sub Total", subTotal));
+        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, -10:N2}\t{3, 11:N2}", "", "", "Tax", tax));
+        //sb.AppendLine(string.Format("{0, -13}\t{1, 8}\t{2, -10:N2}\t{3, 11:N2}", "", "", "Total", total));
+
+        WriteLine("The Report");
+        WriteLine(sb.ToString());
     }
 }
